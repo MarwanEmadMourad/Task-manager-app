4669fabe5cf0102fce9e6b4d6dcf1d22cb6a3daf
MarwanEmadMourad <marwanemad36@gmail.com>
2021-11-02 20:54:05 +0200

    Quering mongodb using find and findOne.

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,10 @@
 // $ git push -u origin main
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+
+// object destructing
+const {MongoClient , ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
-
 
 MongoClient.connect(connectionURL,{ useNewUrlParser:true } , (error,client) =>{
     if (error){
@@ -12,19 +12,22 @@ MongoClient.connect(connectionURL,{ useNewUrlParser:true } , (error,client) =>{
     }
     const db = client.db(databaseName)
     
-    db.collection('Tasks').insertMany([{
-        description:'' ,
-        completed:false 
-    },{
-        description:'' ,
-        completed:false     
-    },{
-        description:'' ,
-        completed:false 
-    }],(error,result) =>{
-        if (error){
-            return console.log('Unable To insert Documents')
-        } 
-        // console.log(result)
+    // finding one element (task with description equals Play Chess)
+    db.collection('Tasks').findOne({ description:'Play Chess'} , (error,task) =>{
+        if (error) {
+            return console.log("Unable to find task")
+        }
+
+        console.log(task)
+    })
+
+    // finding all tasks with completed property equal true
+    db.collection('Tasks').find({completed : false}).toArray((error,tasks) =>{
+        console.log(tasks)
+    })
+
+    // getting the document count of documents with false completed property
+    db.collection('Tasks').find({completed : false}).count((error,count) =>{
+        console.log(count)
     })
 })
