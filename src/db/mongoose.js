6679e0e85cb905c5d96b78f5ef9d713d4153cbff
MarwanEmadMourad <marwanemad36@gmail.com>
2021-11-03 19:13:25 +0200

    Using mongoos built-in validators and custom validators in our model.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,18 +1,48 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api')
 
 // creating user model 
 const User = mongoose.model('User', {
     name: {
-        type:String
+        type:String,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true ,
+        minLength: 7,
+        validate (value) {
+            if (value.toLowerCase().includes('password') === true){
+                throw new Error ('Password can not contain the word "password"')
+            }
+        }
+    },
+    Email:{
+        type: String,
+        required: true,
+        trim: true,
+        lowerCase: true,
+        validate (value) {
+            if (!validator.isEmail(value)){
+                throw new Error ('Enter a valid email.')
+            }
+        }
     },
     age: {
-        type: Number
+        type: Number ,
+        default: 0 ,
+        validate (value) {
+            if (value < 0) {
+                throw new Error ('Age must be positive')
+            }
+        }
     }
 })
 
-const me = new User( { name :'Marwan' , age : 22} )
+const me = new User( { password :'dpassword' , Email:" marwanEmad@yahoo.com "} )
 
 me.save().then((data) =>{
     console.log(data)
@@ -24,14 +54,17 @@ me.save().then((data) =>{
 // creating task model
 const Task = mongoose.model('Task',{
     description:{
-        type: String
+        type: String,
+        trim: true,
+        required:true
     },
     completed:{
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
-const myTask = new Task({description: 5 , completed: 'djkdshfjks'})
+const myTask = new Task({description: "Do some shit" })
 
 myTask.save().then((data) =>{
     console.log(data)
