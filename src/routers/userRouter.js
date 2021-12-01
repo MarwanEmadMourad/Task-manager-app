9f766a8148f301f8cd27c7276fe9743bc384412c
MarwanEmadMourad <marwanemad36@gmail.com>
2021-12-01 10:39:34 +0200

    adding JSON web token in the singup and login routers.

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -8,8 +8,9 @@ const router = new express.Router()
 router.post('/users' , async (req,res) =>{
     const user = new User(req.body)
     try {
+        const token = await user.generateAuthToken()  
         await user.save() 
-        res.status(201).send(user)
+        res.send( {user , token} )    
     } catch(error){
         res.status(400).send(error)
     }
@@ -18,16 +19,16 @@ router.post('/users' , async (req,res) =>{
 // login route
 router.post('/users/login' , async (req,res) =>{
     try {
-        const user = await User.findByCredetials(req.body.email , req.body.password) 
-        res.send(user)
+        const user = await User.findByCredetials(req.body.email , req.body.password)
+        const token = await user.generateAuthToken()  
+        res.send( {user , token} )
     } catch(error){
         res.status(400).send(error.message)
     }
 })
 
 // reading all users
-router.get('/users', async (req,res) =>{
-    
+router.get('/users', async (req,res) => {
     try {
         const users = await User.find({})
         res.send(users)
